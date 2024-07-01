9e0975ddc4a429ebaf7175808b4f114f9fe3f137
Aviv Baron David <bardaviv@gmail.com>
2024-07-01 18:30:37 +0300

    Initial backend setup

---
+++ server.js
@@ -0,0 +1,6 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello there'))
+app.listen(3030, () => console.log('Server ready at port http://127.0.0.1:3030'))
